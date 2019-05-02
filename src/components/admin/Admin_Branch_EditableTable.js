d5e84347b7a00c20deaad885f963ad87b61e670b
Yinnon Hadad Marliss <yanon.dev@gmai.com>
2019-05-02 20:39:03 +0300

    added missing file

---
+++ src/components/admin/Admin_Branch_EditableTable.js
@@ -0,0 +1,331 @@
+import React, { Component } from "react";
+import {
+  MDBModal,
+  MDBModalBody,
+  MDBInput,
+  MDBBtn,
+  MDBModalFooter,
+  MDBIcon,
+  toast
+} from "mdbreact";
+import { Link } from "react-router-dom";
+import Toaster from "../Toaster";
+import Axios from "axios";
+import { RootUrl } from "../constants";
+import Dragzone from "./Dragzone";
+
+
+class Admin_Branch_EditableTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleteModal: false,
+      editModal: false,
+      focused_index: undefined,
+      name: "",
+      icon: "",
+      href:"",
+      // editValues: { name: "", iconName: "", href: "" },
+      branches: []
+    };
+
+    this.getBranches();
+  }
+
+  handleFiles = files => {
+    this.setState({ file: files[0] });
+  };
+
+  uploadFiles = () => {
+    if (!this.state.file) {
+      return alert("No file");
+    }
+    const data = new FormData();
+    data.append("file", this.state.file);
+    data.append("filename", this.state.file.name);
+    data.append("category", "branches");
+    Axios.post(`${RootUrl}/file`, data)
+      .then(res => {
+        this.setState({ href: res.data.path });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  };
+
+  getBranches = () => {
+    Axios.get(`${RootUrl}/branches`, {
+      params: { category_id: this.props.category_id }
+    })
+      .then(res => {
+        this.setState({ branches: res.data, isLoading: false })
+      })
+      .catch(({ error }) => {
+        this.setState({ error, isLoading: false });
+      });
+  };
+
+  handleToggle = (modal, focused_index = undefined) => e => {
+    this.setState({ [modal]: !this.state[modal], focused_index });
+    if (modal === "editModal") {
+      if (focused_index !== undefined) {
+        debugger;
+        this.setState({ editValues: this.state.branches[focused_index] });
+      } else {
+        this.setState({ editValues: { name: "", iconName: "", href: "" } });
+      }
+    }
+  };
+
+  handleDelete = e => {
+    // delete
+    const id = this.state.branches[this.state.focused_index]._id;
+    Axios.delete(`${RootUrl}/branches`, {
+      params: {
+        id
+      }
+    })
+      .then(res => {
+        this.setState({
+          branches: this.state.branches.filter(elem => elem._id !== id),
+          isLoading: false
+        });
+        toast.info("מסמך נמחק בהצלחה!");
+      })
+      .catch(err => {
+        this.setState({ error: err, isLoading: false });
+        toast.error("מחיקת מסמך נכשלה!");
+      });
+
+    this.handleToggle("deleteModal")(e);
+  };
+
+  handleEdit = e => {
+    const { category_id } = this.props;
+    const { focused_index, branches, name, file, icon } = this.state;
+    if (focused_index !== undefined) {
+      if (file) {
+        const data = new FormData();
+        data.append("file", this.state.file);
+        data.append("filename", this.state.file.name);
+        data.append("category", "branches");
+        Axios.post(`${RootUrl}/file`, data).then(res => {
+          const href = res.data.path;
+          Axios.patch(`${RootUrl}/branches`, {
+            category_id,
+            id: branches[focused_index]._id,
+            name,
+            icon,
+            href
+          })
+            .then(res => {
+              branches[focused_index] = res.data;
+
+              this.setState({ branches, isLoading: false });
+              toast.info("מסמך עודכן בהצלחה!");
+            })
+            .catch(err => {
+              this.setState({ error: err, isLoading: false });
+              toast.error("עדכון מסמך נכשל!");
+            });
+        });
+      } else {
+        let { href } = this.state;
+        Axios.patch(`${RootUrl}/branches`, {
+          category_id,
+          id: branches[focused_index]._id,
+          name,
+          icon,
+          href
+        })
+          .then(res => {
+            branches[focused_index] = res.data;
+
+            this.setState({ branches, isLoading: false });
+            toast.info("מסמך עודכן בהצלחה!");
+          })
+          .catch(err => {
+            this.setState({ error: err, isLoading: false });
+            toast.error("עדכון מסמך נכשל!");
+          });
+      }
+    } else {
+      const data = new FormData();
+      data.append("file", this.state.file);
+      data.append("filename", this.state.file.name);
+      data.append("category", "branches");
+      Axios.post(`${RootUrl}/file`, data)
+        .then(res => {
+          const href = res.data.path;
+          Axios.post(`${RootUrl}/branches`, {
+            category_id,
+            icon,
+            name,
+            href
+          })
+            .then(res => {
+              branches.push(res.data);
+
+              this.setState({ branches, isLoading: false });
+              toast.info("מסמך נוסף בהצלחה!");
+            })
+            .catch(err => {
+              this.setState({ error: err, isLoading: false });
+              toast.error(" הוספת מסמך נכשלה!");
+            });
+        })
+        .catch(error => {
+          this.handleToggle("editModal")(e);
+          this.setState({ error });
+          toast.error(" הוספת מסמך נכשלה!");
+          return;
+        });
+    }
+    this.handleToggle("editModal")(e);
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({  [name]: value  });
+  };
+
+  render() {
+    const products = this.state.branches.map(({ name }, index) => {
+      return (
+        <tr>
+          <td className="table-text" scope="row">
+            {index}
+          </td>
+          <td className="table-text">{name}</td>
+          <td className="table-edit-delete text-center">
+            <Link
+              rounded
+              onClick={this.handleToggle("editModal", index)}
+              to="#"
+            >
+              <i className="fas fa-edit" />
+            </Link>
+          </td>
+          <td className="table-edit-delete text-center">
+            <Link
+              rounded
+              onClick={this.handleToggle("deleteModal", index)}
+              to="#"
+            >
+              <i className="fas fa-trash-alt red-text" />
+            </Link>
+          </td>
+        </tr>
+      );
+    });
+    return (
+      <React.Fragment>
+        <table className="table table-hover table-striped">
+          <thead className="table-head">
+            <tr>
+              <th className="table-head" scope="col" width="10%">
+                #
+              </th>
+              <th className="table-head" scope="col" width="70%">
+                שם הטופס
+              </th>
+              <th className="table-head  text-center" scope="col" width="10%">
+                עריכה
+              </th>
+              <th className="table-head text-center" scope="col" width="10%">
+                מחיקה
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td
+                colspan="100%"
+                style={{ color: "green", cursor: "pointer" }}
+                onClick={this.handleToggle("editModal")}
+              >
+                <MDBIcon icon="plus" size="lg" />
+              </td>
+            </tr>
+            {products}
+          </tbody>
+        </table>
+        <MDBModal
+          className="form-elegant "
+          isOpen={this.state.deleteModal}
+          toggle={this.handleToggle("deleteModal")}
+        >
+          <MDBModalBody style={{ textAlign: "center" }}>
+            <h4>
+              האם אתה בטוח שאתה רוצה למחוק? <br />
+              <strong style={{ fontWeight: "900" }}>
+                אין דרך לשחזר פעולה זאת!
+              </strong>
+            </h4>
+            <br />
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn onClick={this.handleToggle("deleteModal")}>ביטול</MDBBtn>
+            <MDBBtn onClick={this.handleDelete} color="red">
+              מחיקה
+            </MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+
+        <MDBModal
+          className="form-elegant"
+          isOpen={this.state.editModal}
+          toggle={this.handleToggle("editModal")}
+        >
+          <MDBModalBody className="mx-3">
+            <section className="form-elegant">
+              <div className="text-center">
+                <h1 className="dark-grey-text mb-5">
+                  <strong>ערוך תוצר הדרכה</strong>
+                </h1>
+              </div>
+
+              <MDBInput
+                name="name"
+                onChange={this.handleChange}
+                label="הזן שם מסמך"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                value={this.state.name}
+              />
+              <MDBInput
+                name="icon"
+                onChange={this.handleChange}
+                label="הזן שם צלמית"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+                value={this.state.icon}
+              />
+              <Dragzone
+                handleFiles={this.handleFiles}
+                file={this.state.file}
+                href={this.state.href}
+              />
+              <div className="mb-3 pr-5 pl-5">
+                <MDBBtn type="button" onClick={this.handleToggle("editModal")}>
+                  <MDBIcon icon="times" size="lg" />
+                </MDBBtn>
+                <MDBBtn type="button" color="green" onClick={this.handleEdit}>
+                  <MDBIcon icon="save" size="lg" />
+                </MDBBtn>
+              </div>
+            </section>
+          </MDBModalBody>
+        </MDBModal>
+        <Toaster />
+      </React.Fragment>
+    );
+  }
+}
+export default Admin_Branch_EditableTable;
