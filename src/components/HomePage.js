0e58f8c2be498e44de9eba1848bd8731fabe5381
Gil Shafat <gish43@gmail.com>
2019-06-28 21:48:00 +0300

    ...

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
     MDBCol,
     MDBRow,
@@ -19,7 +19,7 @@ import "../assets/Lightbox.css";
 import BranchList from "./BranchList";
 import CommanderWord from './commander-word/CommanderWord';
 import Axios from "axios";
-import {RootUrl} from "./constants";
+import { RootUrl } from "./constants";
 
 class HomePage extends Component {
     constructor(props) {
@@ -34,7 +34,7 @@ class HomePage extends Component {
             collapseID: "",
             photoIndex: 0,
             isOpen: false,
-            ideals: [],
+            ideals: [{ name: "aaa", text: "bbb" }, { name: "ccc", text: "ddd" }],
             branches: [],
             updates: [],
             words: [],
@@ -51,20 +51,20 @@ class HomePage extends Component {
         Axios.get(`${RootUrl}/news`)
             .then(response => {
                 console.log(response.data);
-                this.setState({updates: response.data, isLoading: false});
+                this.setState({ updates: response.data, isLoading: false });
             })
             .catch(() => {
-                this.setState({error: "Error fetching posts", isLoading: false});
+                this.setState({ error: "Error fetching posts", isLoading: false });
             });
     };
 
     getBranches = () => {
         Axios.get(`${RootUrl}/branch`)
             .then(res =>
-                this.setState({branches: res.data.branches, isLoading: false})
+                this.setState({ branches: res.data.branches, isLoading: false })
             )
-            .catch(({error}) => {
-                this.setState({error, isLoading: false});
+            .catch(({ error }) => {
+                this.setState({ error, isLoading: false });
             });
     };
 
@@ -72,10 +72,10 @@ class HomePage extends Component {
         Axios.get(`${RootUrl}/commanderwords`)
             .then(res => {
                 console.log(res.data);
-                this.setState({words: res.data, isLoading: false});
+                this.setState({ words: res.data, isLoading: false });
             })
-            .catch(({error}) => {
-                this.setState({error, isLoading: false});
+            .catch(({ error }) => {
+                this.setState({ error, isLoading: false });
             });
     }
 
@@ -85,7 +85,7 @@ class HomePage extends Component {
 
     renderImages = () => {
         let photoIndex = -1;
-        const {images} = this.state;
+        const { images } = this.state;
 
         return images.map(imageSrc => {
             photoIndex++;
@@ -98,7 +98,7 @@ class HomePage extends Component {
                             alt="Gallery"
                             className="img-fluid"
                             onClick={() =>
-                                this.setState({photoIndex: privateKey, isOpen: true})
+                                this.setState({ photoIndex: privateKey, isOpen: true })
                             }
                         />
                     </figure>
@@ -110,31 +110,15 @@ class HomePage extends Component {
     getIdeals = () => {
         Axios.get(`${RootUrl}/ideals`)
             .then(response => {
-                this.setState({ideals: response.data});
+                this.setState({ ideals: response.data });
             })
             .catch(() => {
-                this.setState({error: "Error fetching posts", isLoading: false});
+                this.setState({ error: "Error fetching posts", isLoading: false });
             });
     };
 
-    renderIdeals() {
-        var idealsElements = this.state.ideals.map(ideal => (
-            <MDBCol style={{backgroundColor: "#ede7f6"}}>
-                <MDBRow center>
-                    <h3 className="font-weight-bold mb-3 mt-2 p-0">
-                        <strong>{ideal.name}</strong>
-                    </h3>
-                </MDBRow>
-                <MDBRow center>
-                    <h5 style={{padding: "5%"}}>{ideal.text}</h5>
-                </MDBRow>
-            </MDBCol>
-        ));
-        return idealsElements;
-    }
-
     render() {
-        const {updates, words, branches, isLoading, error} = this.state;
+        const { updates, words, branches, isLoading, error } = this.state;
 
         if (isLoading) {
             return <div>טוען</div>;
@@ -158,44 +142,37 @@ class HomePage extends Component {
                             </MDBCol>
                         </MDBRow>
                         <div className="right-hex">
-                            <img src={this.state.rightHex} height={100} width={100} align="right"/>
+                            <img src={this.state.rightHex} height={100} width={100} align="right" />
                         </div>
                         <div className="left-hex">
-                            <img src={this.state.leftHex} height={100} width={100} align="left"/>
+                            <img src={this.state.leftHex} height={100} width={100} align="left" />
                         </div>
                         <div className="blue-hex">
                             <img src={this.state.blueHex} height={100} width={100} />
                         </div>
                         <div className="home-page-characters">
-                            <img src={this.state.characters} width={400} height={400}/>
+                            <img src={this.state.characters} width={400} height={400} />
                         </div>
                     </MDBView>
 
                 </div>
-                <BranchList branches={branches}/>
-                <div className="commander-words">
-                    {words.map(word => <CommanderWord />)}
-                </div>
+                <BranchList branches={branches} />
                 <h2
                     className="text-right headline-text-color"
-                    style={{marginRight: "2%", marginTop: "3%"}}
+                    style={{ marginRight: "2%", marginTop: "3%" }}
                 >
                     :חדשות המערך
                 </h2>{" "}
-                <MDBRow center style={{margin: "1%", direction: "rtl"}}>
-                    <UpdateBox updates={updates}/>
+                <MDBRow center style={{ margin: "1%", direction: "rtl" }}>
+                    <UpdateBox updates={updates} />
                 </MDBRow>
-                <MDBCard
-                    className="px-3 "
-                    style={{
-                        marginRight: "2%",
-                        marginLeft: "2%",
-                        marginBottom: "2%",
-                        direction: "rtl"
-                    }}
-                >
-                    <MDBRow className="text-center ">{this.renderIdeals()}</MDBRow>
-                </MDBCard>
+                <div className="commander-words">
+                    {words.map(word => {
+                        return <CommanderWord title={word.title}
+                            content={word.content}
+                        />
+                    })}
+                </div>
             </React.Fragment>
         );
     }
--- src/components/commander-word/CommanderWord.js
+++ src/components/commander-word/CommanderWord.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const CommanderWords = props => {
     return (
-        <div className="test">test6</div>
+        <div className="words-wrapper">
+            <div className="title">{props.title}</div>
+            <div className="content"><pre>{props.content}</pre></div>
+        </div>
     );
 };
 
