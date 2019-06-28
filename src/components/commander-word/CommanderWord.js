c0089f8483a6ede59f6fa3c9cd50c32965a0c7ae
Gil Shafat <gish43@gmail.com>
2019-06-29 00:26:56 +0300

    Added the correct pictures in place.

--- src/components/commander-word/CommanderWord.js
+++ src/components/commander-word/CommanderWord.js
@@ -1,12 +1,35 @@
 
 import React from 'react';
 import "./CommanderWord.css";
+import spaceship from '../../images/commander-words/spaceship.png';
+import looking from '../../images/commander-words/looking.png';
+import running from '../../images/commander-words/running.png';
+
+const isReverse = title => title === 'ייעוד המערך' ? '-reverse' : '';
+
+const getImage = title => {
+    switch(title) {
+        case 'חזון המערך':
+            return {src: looking, decription: 'looking'};
+        case 'ייעוד המערך':
+            return {src: running, decription: 'running'};
+        case 'דבר המפקד':
+            return {src: spaceship, decription: 'spaceship'};
+        default:
+            return '';
+    }
+}
 
 const CommanderWords = props => {
     return (
-        <div className="words-wrapper">
-            <div className="title">{props.title}</div>
-            <div className="content"><pre>{props.content}</pre></div>
+        <div className={`commander-word-container${isReverse(props.title)}`}>
+            <div className="words-wrapper">
+                <div className="title">{props.title}</div>
+                <div className="content"><pre>{props.content}</pre></div>
+            </div>
+            <div className="image-wrapper">
+                <img src={getImage(props.title).src} className={`image ${getImage(props.title).decription}`} />
+            </div>
         </div>
     );
 };
